7faccc4f1d5dbc8a0721ba2eed64f61e53571799
gautamaaryak <gautamaaryak@gmail.com>
2021-04-05 00:12:45 +0530

    Initial commit

---
+++ Recommender.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+
+
+
+# Load user_final_rating.csv
+
+ufr = pd.read_csv('user_final_rating.csv')
+ufr.set_index('reviews_username', inplace=True)
+
+
+def get_products(user_input):
+   
+    d = pd.DataFrame(ufr.loc[user_input].sort_values(ascending=False)[0:5]).reset_index()
+
+    return_list = d['index'].values
+    return list(return_list)
+
+if __name__=='__main__':
+    r = get_products('01impala')
+    print(r)
+
+
+
